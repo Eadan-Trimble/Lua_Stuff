a1092adaf8efcc057da26500e71b87f3e7863b95
Ryzid <aqwphishing@gmail.com>
2020-10-25 04:56:20 -0400

    Update animals.lua

--- Fub/Games/animals.lua
+++ Fub/Games/animals.lua
@@ -14,31 +14,31 @@ RemoteEvent:FireServer(
 
 local Event = game:GetService("ReplicatedStorage").RemoteEvent
 
-local p2_hahanigger = _G.tbl or false;
+local p2_haha = _G.tbl or false;
 spawn(function()
     for i,v in pairs(getgc()) do
-    if not p2_hahanigger then
+    if not p2_haha then
         if type(v) == 'function' then
             for up,value in pairs(debug.getupvalues(v)) do
                 if type(value) == 'table' then
                     if rawget(value,'animalStats') and rawget(value,'animalModels') then
-                        p2_hahanigger = value
-                        return print('Found',p2_hahanigger)
+                        p2_haha = value
+                        return print('Found',p2_haha)
                     end
                 end
             end
         elseif type(v) == 'table' then
             if rawget(v,'animalStats') and rawget(v,'animalModels') then
-                p2_hahanigger = v
-                return print('Found',p2_hahanigger)
+                p2_haha = v
+                return print('Found',p2_haha)
             end
         end
     else
         return
     end
 end end)
-repeat wait() until p2_hahanigger
-yes = p2_hahanigger
+repeat wait() until p2_haha
+yes = p2_haha
 tbl(yes,false,'Animal Functions Hidden')
 print(yes.animalSelected)
 game:GetService'RunService'.RenderStepped:connect(function()
